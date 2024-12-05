9c770fa8b0788e0d63b80b1ef789ba7ac95e4e68
Sascha Ziegler <sascha8398@googlemail.com>
2024-12-05 19:53:53 +0100

    changed alignment comment

--- lib/features/article_comments/widgets/comment_widget.dart
+++ lib/features/article_comments/widgets/comment_widget.dart
@@ -14,6 +14,7 @@ class CommentWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Row(
           children: [
