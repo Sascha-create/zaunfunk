bfea9166f14281c822ab969a804f99c73c546d7e
Sascha Ziegler <sascha8398@googlemail.com>
2024-12-10 15:17:42 +0100

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:zaunfunk/firebase_options.dart';
 import 'package:zaunfunk/shared/config/themes.dart';
 import 'package:zaunfunk/features/authentication/screens/login_screen.dart';
 import 'package:zaunfunk/shared/repositories/database_repository.dart';
 import 'package:zaunfunk/shared/repositories/mock_database.dart';
 
-void main() {
+// com.example.zaunfunk  muss vor eventuellem hochladen geändert werden !
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   final DatabaseRepository repository = MockDatabase();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(MainApp(repository: repository,));
 }
 
