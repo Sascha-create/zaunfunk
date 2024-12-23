39ae532f182abc6d415c449bf17d5f57405da763
Sascha Ziegler <sascha8398@googlemail.com>
2024-12-23 15:23:40 +0100

    added RegExp to Email Validator

--- lib/features/authentication/validator.dart
+++ lib/features/authentication/validator.dart
@@ -1,28 +1,29 @@
 String? isValidUsername(String? value) {
-    if (value == null) {
-      return "kein Nutzername angegeben";
-    }
-    if (value.length < 2) return "Nutzername mehr als 2 Zeichen";
-    if (value.length > 20) return "Nutzername maximal 20 Zeichen";
-
-    return null;
+  if (value == null) {
+    return "kein Nutzername angegeben";
   }
+  if (value.length < 2) return "Nutzername mehr als 2 Zeichen";
+  if (value.length > 20) return "Nutzername maximal 20 Zeichen";
 
-  String? isEmailValid(String? value) {
-    if (value == null) return "keine Email";
-    if (value.length < 5) return "Email zu kurz";
-    if (value.length > 30) return "Email zu lang";
-    if (value.contains(",")) return "Darf kein , enthalten";
-    if (value.contains("@")) return null;
-    if (value.contains(".")) return null;
+  return null;
+}
+
+// Inspired by dart_fast and https://stackoverflow.com/a/50663835
+String? isEmailValid(String? value) {
+  if (value == null) return "keine Email angegeben";
+  if (RegExp(
+          r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+      .hasMatch(value)) {
     return null;
+  } else {
+    return "Keine gültige Email angegeben";
   }
+}
 
-  String? isValidPassword(String? value) {
-    // null muss geprüft immer werden
-    if (value == null) return "kein Passwort angegeben";
-    if (value.length < 5) return "Passwort mehr als 5 Zeichen";
-    if (value.length > 20) return "Passwort maximal 20 Zeichen";
-    return null;
-  }
-
+String? isValidPassword(String? value) {
+  // null muss geprüft immer werden
+  if (value == null) return "kein Passwort angegeben";
+  if (value.length < 5) return "Passwort mehr als 5 Zeichen";
+  if (value.length > 20) return "Passwort maximal 20 Zeichen";
+  return null;
+}
